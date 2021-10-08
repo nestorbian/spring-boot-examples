5ce5775870bb5c0c7b92826ad7c2e3241c2639f4
nestor.bian <1756073941@q.com>
2021-10-08 21:30:42 +0800

    更新

--- mybatis-demo/src/main/java/com/nestor/mybatisdemo/MybatisDemoApplication.java
+++ mybatis-demo/src/main/java/com/nestor/mybatisdemo/MybatisDemoApplication.java
@@ -1,17 +1,35 @@
 package com.nestor.mybatisdemo;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 @MapperScan("com.nestor.mybatisdemo.mapper")
 @EnableTransactionManagement
-public class MybatisDemoApplication {
+public class MybatisDemoApplication implements ApplicationRunner {
+
+    @Autowired
+    private DataSource dataSource;
 
     public static void main(String[] args) {
         SpringApplication.run(MybatisDemoApplication.class, args);
     }
 
+    /**
+     * Callback used to run the bean.
+     *
+     * @param args incoming application arguments
+     * @throws Exception on error
+     */
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.err.println(dataSource.getClass().getCanonicalName());
+    }
 }
--- spring-expression/src/main/java/com/nestor/springexpression/SpringExpressionApplication.java
+++ spring-expression/src/main/java/com/nestor/springexpression/SpringExpressionApplication.java
@@ -1,13 +1,41 @@
 package com.nestor.springexpression;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringExpressionApplication {
+public class SpringExpressionApplication implements ApplicationRunner {
+
+	@Value("${boy-name:}")
+	private String boyName;
+
+	@Value("${boy-age:}")
+	private String boyAge;
+
+	@Value("${boy-score:noValue}")
+	private String boyScore;
+
+	@Value("${boy-address:}")
+	private String boyAddress;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringExpressionApplication.class, args);
 	}
 
+	/**
+	 * Callback used to run the bean.
+	 *
+	 * @param args incoming application arguments
+	 * @throws Exception on error
+	 */
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		System.err.println(boyAge);
+		System.err.println(boyName);
+		System.err.println(boyScore);
+		System.err.println(boyAddress);
+	}
 }
